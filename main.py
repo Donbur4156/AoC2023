352e179ddd6e0c89a0a9ef8fd89b361aca612383
jplight <janpfister@t-online.de>
2023-12-02 13:25:27 +0100

    adding some range checks

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ import shutil
 
 
 if len(sys.argv) == 1:
+    today = datetime.datetime.today()
+    if today.year != 2023 or today.month != 12:
+        print('Error: can\'t run without specific day outside of competition time')
+        exit(1)
     target_day = str(datetime.datetime.today().day)
 else:
     target_day = sys.argv[1]
+
+if int(target_day) > datetime.datetime.today().day:
+    print('Error: Can\'t run future day')
+    exit(1)
 
 target_day = target_day.zfill(2)
 day_data = get_day_data(target_day)
